09b6c460f293f727d7f43d0e7bd6a61d56e6f61a
OscSta <oscstal@gmail.com>
2025-11-29 00:15:10 +0100

    yt search api fetch with search term

--- main.py
+++ main.py
@@ -2,9 +2,16 @@ import dotenv
 import os
 import logging
 import discord
+import requests
+import json
 
 def setup_logging():
     logging.basicConfig(level=logging.INFO)
+
+def query_yt_search_api_with(**search_args):
+    base = "https://www.googleapis.com/youtube/v3/search"
+    result = requests.get(base, params=search_args)
+    return result
 
 def main():
     dotenv.load_dotenv()
@@ -22,6 +29,8 @@ def main():
     @discord.option("search", input_type=str)
     async def music(ctx: discord.ApplicationContext, search: str):
         await ctx.respond(f"Pong! Latency is {bot.latency} and search term is {search}")
+        response = query_yt_search_api_with(part="snippet", maxResults=5, regionCode="SE", safeSearch="none", type="video", q=search, key=os.getenv("YT_API_KEY"))
+        print(json.dumps(response.json(), indent=2))
 
 
     bot.run(os.getenv("DISCORD_BOT_TOKEN"))
